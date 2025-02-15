960255a102aeb183bd7678c5eb1a26d87d61da77
imblowsnow <im.blowsnow@gmail.com>
2025-02-15 12:35:51 +0800

    feat: init

--- test/buildResource.ts
+++ test/buildResource.ts
@@ -1,15 +1,8 @@
-import ModuleLoadUtils from "../nodes/help/utils/ModuleLoadUtils";
-import ResourceBuilder from "../nodes/help/builder/ResourceBuilder";
-import {ResourceOperations} from "../nodes/help/type/IResource";
+import ResourceFactory from '../nodes/help/builder/ResourceFactory';
 
-const resourceBuilder = new ResourceBuilder();
-
-ModuleLoadUtils.loadModules(__dirname + "/../dist/nodes/WechatOfficialAccountNode", 'resource/*.js').forEach((resource) => {
-	resourceBuilder.addResource(resource);
-	ModuleLoadUtils.loadModules(__dirname + "/../dist/nodes/WechatOfficialAccountNode", `resource/${resource.value}/*.js`).forEach((operate: ResourceOperations) => {
-		resourceBuilder.addOperateT(resource.value, operate);
-	})
-});
+const resourceBuilder = ResourceFactory.build(
+	__dirname + '/../dist/nodes/FeishuNode',
+);
 
 let txt = '';
 for (let resource of resourceBuilder.resources) {
