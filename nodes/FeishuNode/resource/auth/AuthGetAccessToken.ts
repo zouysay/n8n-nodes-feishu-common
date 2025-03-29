8e114f1b8bd78933b1b796b1e0505982b5e11631
imblowsnow <im.blowsnow@gmail.com>
2025-03-29 21:34:28 +0800

    fix：修复报错提示

--- nodes/FeishuNode/resource/auth/AuthGetAccessToken.ts
+++ nodes/FeishuNode/resource/auth/AuthGetAccessToken.ts
@@ -1,11 +1,18 @@
 import { IDataObject, IExecuteFunctions } from 'n8n-workflow';
 import { ResourceOperations } from '../../../help/type/IResource';
+import RequestUtils from '../../../help/utils/RequestUtils';
 
 export default {
 	name: '获取当前应用AccessToken',
 	value: 'auth:getAccessToken',
 	options: [],
 	async call(this: IExecuteFunctions, index: number): Promise<IDataObject> {
+
+		await RequestUtils.request.call(this, {
+			method: 'GET',
+			url: `/open-apis/event/v1/outbound_ip`,
+		});
+
 		const credentials = await this.getCredentials('feishuCredentialsApi');
 
 		return {
--- nodes/help/utils/RequestUtils.ts
+++ nodes/help/utils/RequestUtils.ts
@@ -1,4 +1,4 @@
-import { IExecuteFunctions, NodeOperationError } from 'n8n-workflow';
+import { IExecuteFunctions } from 'n8n-workflow';
 import { IRequestOptions } from 'n8n-workflow/dist/Interfaces';
 
 class RequestUtils {
@@ -9,15 +9,25 @@ class RequestUtils {
 	) {
 		const credentials = await this.getCredentials('feishuCredentialsApi');
 
-		return this.helpers.requestWithAuthentication.call(this, 'feishuCredentialsApi', options, {
-			// @ts-ignore
-			credentialsDecrypted: {
-				data: {
-					...credentials,
-					accessToken: clearAccessToken ? '' : credentials.accessToken,
+		return this.helpers.requestWithAuthentication
+			.call(this, 'feishuCredentialsApi', options, {
+				// @ts-ignore
+				credentialsDecrypted: {
+					data: {
+						...credentials,
+						accessToken: clearAccessToken ? '' : credentials.accessToken,
+					},
 				},
-			},
-		});
+			})
+			.catch((err) => {
+				if (err?.cause?.error) {
+					throw new Error(
+						`Request Error: ${err?.cause?.error.code}, ${err?.cause?.error.msg} \n ` +
+							JSON.stringify(err?.cause?.error.error),
+					);
+				}
+				throw err;
+			});
 	}
 
 	static async request(this: IExecuteFunctions, options: IRequestOptions) {
@@ -26,7 +36,9 @@ class RequestUtils {
 		return RequestUtils.originRequest.call(this, options).then((data) => {
 			const handleResponse = (data: any) => {
 				if (data.code && data.code !== 0) {
-					throw new NodeOperationError(this.getNode(), `Request Error: ${data.code}, ${data.msg} \n ` + JSON.stringify(data.error));
+					throw new Error(
+						`Request Error: ${data.code}, ${data.msg} \n ` + JSON.stringify(data.error),
+					);
 				}
 				return data;
 			};
