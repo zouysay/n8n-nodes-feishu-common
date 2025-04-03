d849c06c7730aa24098b974277e2e5dbfa6cf43a
imblowsnow <im.blowsnow@gmail.com>
2025-04-03 15:03:42 +0800

    feat: 获取accesstoken节点增加描述

--- nodes/FeishuNode/resource/auth/AuthGetAccessToken.ts
+++ nodes/FeishuNode/resource/auth/AuthGetAccessToken.ts
@@ -4,6 +4,7 @@ import RequestUtils from '../../../help/utils/RequestUtils';
 
 export default {
 	name: '获取当前应用AccessToken',
+	description: '需开通出口ip权限',
 	value: 'auth:getAccessToken',
 	options: [],
 	async call(this: IExecuteFunctions, index: number): Promise<IDataObject> {
